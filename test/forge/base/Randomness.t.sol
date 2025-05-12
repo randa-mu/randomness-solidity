0e340a31040382818e7f3cc8e65b6ea295a33f8c
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-05-12 11:27:38 +0100

    Add test for update signature scheme

--- test/forge/base/Randomness.t.sol
+++ test/forge/base/Randomness.t.sol
@@ -44,4 +44,38 @@ contract RandomnessTest is Deployment {
         console.logBytes(bn254SignatureScheme.DST());
         console.logString(string(bn254SignatureScheme.DST()));
     }
+
+    function test_Update_SignatureScheme() public {
+        // non-zero address with zero code
+        string memory bn254_schemeID = "BN255";
+        address schemeAddr = makeAddr(bn254_schemeID);
+        assertTrue(schemeAddr != address(0), "schemeAddr should not be zero address");
+        assertTrue(schemeAddr.code.length == 0, "schemeAddr should not have any code");
+        vm.prank(admin);
+        vm.expectRevert("Invalid contract address for schemeAddress");
+        signatureSchemeAddressProvider.updateSignatureScheme(bn254_schemeID, schemeAddr);
+
+        // non-zero address with non-zero code
+        schemeAddr = address(bn254SignatureScheme);
+        assertTrue(schemeAddr != address(0), "schemeAddr address should not be zero address");
+        assertTrue(schemeAddr.code.length > 0, "schemeAddr address should have code");
+        vm.prank(admin);
+        signatureSchemeAddressProvider.updateSignatureScheme(bn254_schemeID, schemeAddr);
+        assertTrue(signatureSchemeAddressProvider.getSignatureSchemeAddress(bn254_schemeID) == schemeAddr);
+
+        // replacing existing scheme contract reverts
+        schemeAddr = address(randomnessSender);
+        vm.prank(admin);
+        vm.expectRevert("Scheme already added for schemeID");
+        signatureSchemeAddressProvider.updateSignatureScheme(bn254_schemeID, schemeAddr);
+        assertTrue(
+            signatureSchemeAddressProvider.getSignatureSchemeAddress(bn254_schemeID) != schemeAddr,
+            "Scheme contract address should not have been replaced"
+        );
+
+        // zero address with zero code
+        vm.prank(admin);
+        vm.expectRevert("Invalid contract address for schemeAddress");
+        signatureSchemeAddressProvider.updateSignatureScheme(bn254_schemeID, address(0));
+    }
 }
