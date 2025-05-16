4c17b68c08e095727f6f72163eb5003a605c3fe8
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-05-16 03:00:13 +0100

    fix linting

--- src/libraries/BLS.sol
+++ src/libraries/BLS.sol
@@ -458,7 +458,7 @@ library BLS {
     ///     in rfc9380 section 5.3.1, using H = keccak256.
     /// @param DST Domain separation tag
     /// @param message The message to expand
-    /// @param n_bytes The number of bytes to extend to    
+    /// @param n_bytes The number of bytes to extend to
     function expandMsg(bytes memory DST, bytes memory message, uint8 n_bytes) internal pure returns (bytes memory) {
         uint256 domainLen = DST.length;
         if (domainLen > 255) {
